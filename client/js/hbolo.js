0691595c214326fa9566a8f1752aa7eb9db56009
Brian Wigginton <brianwigginton@gmail.com>
2010-09-19 21:53:18 -0600

    Updated game architecture

--- client/js/hbolo.js
+++ client/js/hbolo.js
@@ -1,6 +1,28 @@
-var hbolo = hbolo || function() {
+var hbolo = hbolo || {};
+
+hbolo = function() {
+
+	var canvas = document.getElementById("canvas").getContext('2d');
+	var input = new hbolo.InputManager();	
+	var map = new hbolo.MappingSystem('shitbrains');
 	
-	var ctx = document.getElementById("canvas").getContext('2d');
-	var map = new MappingSystem(ctx, 'shitbrains');
+	return {
+		update: function() {
+			if(input.getKeyStates.quit) {
+				this.end();
+			}
+		},
+		loop: function() {
+			this.update();
+			this.draw();
+		},
+		draw: function() {
+			map.draw(canvas);
+		},
+		end: function() {
+			clearInterval(GameLoop);
+		}
+	};
 	
 };
+var GameLoop = setInterval("hbolo.loop()", 33);
--- client/js/hbolo.mapping_system.js
+++ client/js/hbolo.mapping_system.js
@@ -1,53 +1,49 @@
-var MappingSystem = MappingSystem || function(ctx, mapName) {
+var hbolo = hbolo || {};
+
+hbolo.MappingSystem = function(mapName) {
 	
-	var canvas = ctx,
-			map,
-			digest;
+	var digest = {},
+			path = "/maps/" + mapName + "/",
+			baseX = 0,
+			baseY = 0;
 	
-	var loadMap = function(mapName) {
+	// load the map digest from the server
+	$.ajax({
+		url: path + 'digest.json',
+		success: function(json) {
+			digest = eval('(' + json + ')');
+			digest.map.file = path + "map.png";
+			map = new Image();
+			map.src = digest.map.file;
+		},
+		failure: function() {
+			alert("Couldn't load the map digest\n" + path);
+			GameLoop = false;
+		}
+	});	
+	
+	return {
+		draw: function(canvas) {
 
-		var path = "/maps/" + mapName + "/";
-		
-		// load the map digest from the server
-		$.ajax({
-			url: path + 'digest.json',
-			success: function(json) {
-				digest = eval('(' + json + ')');
-				digest.map.file = path + "map.png";
-				map = new Image();
-				map.src = digest.map.file;
-				map.onload = draw();
-			},
-			failure: function() {
-				alert("Couldn't load the map digest\n" + path);
-			}
-		});
-		
-	};
-		
-	var draw = function() {
-		
-		// loop through each data row
-		for(y in digest.map.layout) {
+			canvas.width = canvas.width;
 
-			// get the number of tiles for the row
-			var numTiles = digest.map.layout[y].length;
-			
-			for(x = 0; x < numTiles; x++) {
+			// loop through each data row
+			for(y in digest.map.layout) {
 
-				var currentTile = digest.map.layout[y].charAt(x),
-						sX = digest.map.tile_mappings[currentTile].split(",")[0],
-						sY = digest.map.tile_mappings[currentTile].split(",")[1],
-						mapX = x * digest.map.tile_size,
-						mapY = y * digest.map.tile_size;
-						
-				// console.log(x + ' ' + currentTile + ": " + sX + "," + sY);
-				// //console.log(x + ' ' + "map: " + mapX + "," + mapY);
-				canvas.drawImage(map, sX, sY, digest.map.tile_size, digest.map.tile_size, mapX, mapY, digest.map.tile_size, digest.map.tile_size);
+				// get the number of tiles for the row
+				var numTiles = digest.map.layout[y].length;
+
+				for(x = 0; x < numTiles; x++) {
+
+					var currentTile = digest.map.layout[y].charAt(x),
+							sX = digest.map.tile_mappings[currentTile].split(",")[0],
+							sY = digest.map.tile_mappings[currentTile].split(",")[1],
+							mapX = baseX + (x * digest.map.tile_size),
+							mapY = baseY + (y * digest.map.tile_size);
+
+					canvas.drawImage(map, sX, sY, digest.map.tile_size, digest.map.tile_size, mapX, mapY, digest.map.tile_size, digest.map.tile_size);
+				}
 			}
 		}
 	};
-
-	
-	loadMap(mapName);
 };
