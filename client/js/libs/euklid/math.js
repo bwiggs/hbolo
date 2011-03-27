28b7303821c98d52442c8e26ef3cb1302e0bfe41
Brian Wigginton <brianwigginton@gmail.com>
2011-03-27 03:14:31 -0600

    moved physics logic out of math.js

--- client/js/libs/euklid/math.js
+++ client/js/libs/euklid/math.js
@@ -16,15 +16,4 @@ Math.Pythagoras = function(x1, y1, x2, y2) {
 	return Math.sqrt(Math.pow((x1-x2), 2) + Math.pow((y1 - y2), 2));
 };
 
-//=============================================================================
-// PHYSICS
-//=============================================================================
 
-var Physics = Physics || {
-  collision: function() {
-    return ((a.r + b.r) > Math.Pythagoras(a.x,a.y,b.x,b.y));
-  },
-  isPolyCollision: function() {
-    return false;
-  }
-};
